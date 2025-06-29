9360eea2f846463433c60936d040de87ac6ec73d
Hades <zstbmw@163.com>
2025-06-29 21:48:01 +0800

    Revert Changes from Force Push

--- services/core/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://postgres:123456@localhost:5432/postgres"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-metadata = MetaData()
--- services/scripts/import_gee.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d E:\pycode\datasourceminioProject
-set HTTP_PROXY=http://127.0.0.1:7890
-set HTTPS_PROXY=http://127.0.0.1:7890
-call E:\anaconda\envs\datasource\python.exe cli.py --operation import_gee --folder_name test
-pause
